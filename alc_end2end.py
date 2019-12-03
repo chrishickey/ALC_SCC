55fa5d34bd4a6968df4a07fbe208e1705360a4e5
Chris Hickey <chris@comcom.ai>
2019-12-03 22:21:36 +0900

    complete test cases

--- alc_end2end.py
+++ alc_end2end.py
@@ -2,45 +2,45 @@ from labeled_graph import LabeledGraph
 from alc_decomposed import AlcDecomposed
 from alc_scc_decomposed import AlcSccDecomposed
 import sys, time, datetime
-
+TEST_LABEL_SIZE = 6
+TEST_SET_SIZE = 1000
 
 
 def main():
     try:
         graph_file = sys.argv[1]
-        # For DFS recursion is very high
-
     except:
         graph_file = 'V5kD5L8exp.edge'
-    start_time = time.time()
+
     labeled_graph = LabeledGraph.get_graph_from_file(graph_file)
-    ttc, ftc = labeled_graph.get_test_cases(label_size=2, number=1000)
+    ttc, ftc = labeled_graph.get_test_cases(label_size=TEST_LABEL_SIZE, number=TEST_SET_SIZE)
     print('Found {} true test cases and {} false test case'.format(len(ttc), len(ftc)))
+
+    start_time = time.time()
     alc_decomposed_graph = AlcDecomposed.decompose_labeled_graph(labeled_graph)
     alc_scc_decomposed_graph = AlcSccDecomposed(alc_decomposed_graph.unlabeledGraphs)
     end_time = time.time()
     print('Construction time is {}'.format(end_time - start_time))
 
+    # Free memory space
+    del alc_decomposed_graph
+    del labeled_graph
 
-    true_answer_times = []
+    start = datetime.datetime.now()
     for source, target, query_set in ttc:
-        start = datetime.datetime.now()
         answer = alc_scc_decomposed_graph.lcr_query(source, target, query_set)
-        end = datetime.datetime.now()
         assert answer == True
-        true_answer_times.append((start - end).microseconds)
-    average = sum(true_answer_times) / len(true_answer_times)
-    print('Average time for true queries {}'.format(average))
+    end = datetime.datetime.now()
+    average = (end - start).microseconds / len(ttc)
+    print('Average time for true queries {} microseconds after {} questions'.format(average, len(ttc)))
 
-    false_answer_times = []
+    start = datetime.datetime.now()
     for source, target, query_set in ftc:
-        start = datetime.datetime.now()
         answer = alc_scc_decomposed_graph.lcr_query(source, target, query_set)
-        end = datetime.datetime.now()
         assert answer == False
-        false_answer_times.append((start - end).microseconds)
-    average = sum(false_answer_times) / len(false_answer_times)
-    print('Average time for false queries {}'.format(average))
+    end = datetime.datetime.now()
+    average = (end - start).microseconds / len(ftc)
+    print('Average time for false queries {} microseconds after {} questions'.format(average, len(ftc)))
 
 
 if __name__ == '__main__':
